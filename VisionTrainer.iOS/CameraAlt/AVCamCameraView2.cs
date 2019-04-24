64383e97e9be4a1ea971e91b68d06cc6a226c558
Clint Francis <15001787+ClintFrancis@users.noreply.github.com>
2019-04-24 12:19:55 +0100

    Cleaning up fields

--- VisionTrainer.iOS/CameraAlt/AVCamCameraView2.cs
+++ VisionTrainer.iOS/CameraAlt/AVCamCameraView2.cs
@@ -17,18 +17,16 @@ namespace Temp
 	{
 		AVCaptureVideoPreviewLayer previewLayer;
 		AVCaptureSession captureSession;
-		AVCaptureDeviceInput captureDeviceInput;
-		AVCaptureStillImageOutput stillImageOutput;
 		AVCapturePhotoOutput photoOutput;
 		CameraOptions cameraOptions;
 
 		//new
-		Dictionary<long, AVCamPhotoCaptureDelegate> inProgressPhotoCaptureDelegates;
-		int inProgressLivePhotoCapturesCount;
 		DispatchQueue sessionQueue;
 
 
 		public event EventHandler<EventArgs> Tapped;
+		public event EventHandler<EventArgs> Captured; // TODO
+
 		public bool IsPreviewing { get; set; }
 
 		public UICameraPreviewAlt(CameraOptions options)
@@ -112,11 +110,13 @@ namespace Temp
 			// Add photo output.
 			photoOutput = new AVCapturePhotoOutput();
 			photoOutput.IsHighResolutionCaptureEnabled = true;
+
+
 			if (captureSession.CanAddOutput(photoOutput))
 			{
 				captureSession.AddOutput(photoOutput);
 				inProgressPhotoCaptureDelegates = new Dictionary<long, AVCamPhotoCaptureDelegate>();
-				inProgressLivePhotoCapturesCount = 0;
+				//inProgressLivePhotoCapturesCount = 0;
 			}
 			else
 			{
@@ -168,109 +168,6 @@ namespace Temp
 			IsPreviewing = false;
 		}
 
-		public Task<byte[]> Capture()
-		{
-			Console.WriteLine("Capture");
-			//return Task.FromResult(new byte[0]);
-
-			sessionQueue.DispatchAsync(() =>
-						{
-
-							// Update the photo output's connection to match the video orientation of the video preview layer.
-							var photoOutputConnection = photoOutput.ConnectionFromMediaType(AVMediaType.Video);
-							//photoOutputConnection.VideoOrientation = videoPreviewLayerVideoOrientation;
-
-							AVCapturePhotoSettings photoSettings;
-							// Capture HEIF photo when supported, with flash set to auto and high resolution photo enabled.
-
-							if (photoOutput.AvailablePhotoCodecTypes.Where(codec => codec == AVVideo2.CodecHEVC).Any())
-							{
-								photoSettings = AVCapturePhotoSettings.FromFormat(new NSDictionary<NSString, NSObject>(AVVideo.CodecKey, AVVideo2.CodecHEVC));
-							}
-							else
-							{
-								photoSettings = AVCapturePhotoSettings.Create();
-							}
-
-							//if (videoDeviceInput.Device.FlashAvailable)
-							//{
-							//	photoSettings.FlashMode = AVCaptureFlashMode.Auto;
-							//}
-							photoSettings.IsHighResolutionPhotoEnabled = true;
-							if (photoSettings.AvailablePreviewPhotoPixelFormatTypes.Count() > 0)
-							{
-								photoSettings.PreviewPhotoFormat = new NSDictionary<NSString, NSObject>(CoreVideo.CVPixelBuffer.PixelFormatTypeKey, photoSettings.AvailablePreviewPhotoPixelFormatTypes.First());
-							}
-
-							photoSettings.IsDepthDataDeliveryEnabled(false);
-
-							// Use a separate object for the photo capture delegate to isolate each capture life cycle.
-							var photoCaptureDelegate = new AVCamPhotoCaptureDelegate(photoSettings, () =>
-										{
-											DispatchQueue.MainQueue.DispatchAsync(() =>
-											{
-												//PreviewView.VideoPreviewLayer.Opacity = 0.0f;
-												//UIView.Animate(0.25, () =>
-												//{
-												//	PreviewView.VideoPreviewLayer.Opacity = 1.0f;
-												//});
-											});
-										}, (bool capturing) =>
-										{
-											/*
-												Because Live Photo captures can overlap, we need to keep track of the
-												number of in progress Live Photo captures to ensure that the
-												Live Photo label stays visible during these captures.
-											*/
-											sessionQueue.DispatchAsync(() =>
-											{
-												if (capturing)
-												{
-													inProgressLivePhotoCapturesCount++;
-												}
-												else
-												{
-													inProgressLivePhotoCapturesCount--;
-												}
-
-												var lInProgressLivePhotoCapturesCount = inProgressLivePhotoCapturesCount;
-												DispatchQueue.MainQueue.DispatchAsync(() =>
-												{
-													//if (lInProgressLivePhotoCapturesCount > 0)
-													//{
-													//	CapturingLivePhotoLabel.Hidden = false;
-													//}
-													//else if (lInProgressLivePhotoCapturesCount == 0)
-													//{
-													//	CapturingLivePhotoLabel.Hidden = true;
-													//}
-													//else
-													//{
-													//	Console.WriteLine(@"Error: In progress live photo capture count is less than 0");
-													//}
-												});
-											});
-										}, (AVCamPhotoCaptureDelegate lPhotoCaptureDelegate) =>
-										{
-											// When the capture is complete, remove a reference to the photo capture delegate so it can be deallocated.
-											sessionQueue.DispatchAsync(() =>
-														{
-															inProgressPhotoCaptureDelegates[lPhotoCaptureDelegate.RequestedPhotoSettings.UniqueID] = null;
-														});
-										});
-
-							/*
-								The Photo Output keeps a weak reference to the photo capture delegate so
-								we store it in an array to maintain a strong reference to this object
-								until the capture is completed.
-							*/
-							inProgressPhotoCaptureDelegates[photoCaptureDelegate.RequestedPhotoSettings.UniqueID] = photoCaptureDelegate;
-							photoOutput.CapturePhoto(photoSettings, photoCaptureDelegate);
-						});
-
-			return Task.FromResult(new byte[0]);
-		}
-
 		public void UpdateCameraOption(CameraOptions option)
 		{
 			Console.WriteLine("UpdateCameraOption");
@@ -281,5 +178,52 @@ namespace Temp
 			base.Dispose(disposing);
 		}
 
+		AVCamPhotoCaptureDelegate photoCaptureDelegate;
+		public Task<byte[]> Capture()
+		{
+			Console.WriteLine("Capture");
+
+			// Update the photo output's connection to match the video orientation of the video preview layer.
+			var photoOutputConnection = photoOutput.ConnectionFromMediaType(AVMediaType.Video);
+			//photoOutputConnection.VideoOrientation = videoPreviewLayerVideoOrientation;
+
+			AVCapturePhotoSettings photoSettings;
+			photoSettings = AVCapturePhotoSettings.FromFormat(new NSDictionary<NSString, NSObject>(AVVideo.CodecKey, AVVideo.CodecJPEG));
+			photoSettings.IsHighResolutionPhotoEnabled = true;
+			photoSettings.IsDepthDataDeliveryEnabled(false);
+
+			if (photoSettings.AvailablePreviewPhotoPixelFormatTypes.Count() > 0)
+			{
+				photoSettings.PreviewPhotoFormat = new NSDictionary<NSString, NSObject>(CoreVideo.CVPixelBuffer.PixelFormatTypeKey, photoSettings.AvailablePreviewPhotoPixelFormatTypes.First());
+			}
+
+			// Use a separate object for the photo capture delegate to isolate each capture life cycle.
+			photoCaptureDelegate = new AVCamPhotoCaptureDelegate(photoSettings, WillCapturePhotoAnimation, LivePhotoCaptureHandler, CompletionHandler);
+
+			/*
+				The Photo Output keeps a weak reference to the photo capture delegate so
+				we store it in an array to maintain a strong reference to this object
+				until the capture is completed.
+			*/
+			photoOutput.CapturePhoto(photoSettings, photoCaptureDelegate);
+
+
+			return Task.FromResult(new byte[0]);
+		}
+
+		void WillCapturePhotoAnimation()
+		{
+
+		}
+
+		void LivePhotoCaptureHandler(bool capturing)
+		{
+
+		}
+
+		void CompletionHandler(AVCamPhotoCaptureDelegate photoCaptureDelegate)
+		{
+
+		}
 	}
 }
