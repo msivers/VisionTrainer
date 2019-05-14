e8ff729264d534d656a3908d1329ae01524d4fee
Clint Francis <15001787+ClintFrancis@users.noreply.github.com>
2019-05-14 12:27:28 +0200

    iOS icons and colors updated

--- VisionTrainer/Constants/AppColors.cs
+++ VisionTrainer/Constants/AppColors.cs
@@ -5,8 +5,8 @@ namespace VisionTrainer.Constants
 {
 	public static class AppColors
 	{
-		public static Color HeaderColor = Color.FromRgb(73, 113, 175); // #4971AF
-		public static Color ButtonColor = Color.FromRgb(237, 237, 237); // #EDEDED
+		public static Color HeaderColor = Color.FromHex("#087EFF"); // #4971AF
+		public static Color ButtonColor = Color.FromHex("#087EFF"); // #EDEDED
 
 	}
 }
